cf1b92ae2ef5cdcf88de3d3ca3ac2a07f2e04cc1
fabolvera <fabolvera@gmail.com>
2024-01-25 19:15:23 -0600

    Update index.js actualizacion hostname

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-//const express = require('express');
-
-//const app = express();
-//app.get('/', (req, res) => {
-//  res.send('Hello new World 2');
-//})
-//module.exports = app;
-
+const express = require('express');
 // Importar el módulo os
 const os = require('os');
- 
 // Obtener el nombre de la máquina
 const hostname = os.hostname();
+
+const app = express();
+app.get('/', (req, res) => {
+ res.send('Hostname:'+ hostname);
+})
+module.exports = app;
+
  
 // Mostrar el nombre de la máquina
-console.log('El nombre de la máquina es:', hostname);
+//console.log('El nombre de la máquina es:', hostname);
